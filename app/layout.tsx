9ee5b83a41f2eeb6e61c1d78e6174b95ad6e6c2c
Samuel Lopes <samuel.gomes@aluno.faculdadeimpacta.com.br>
2026-05-06 14:35:18 -0300

    adiciona verificação do google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Partiuuferias - Consultor de Viagens",
-  description: "Especialista em viagens personalizadas — passagens, hotéis, cruzeiros e muito mais. Atendimento exclusivo do início ao fim para você viajar com tranquilidade.",
+  description: "Especialista em viagens personalizadas — passagens, hotéis, cruzeiros e muito mais.",
+  verification: {
+    google: "taIPR0W2l_8P5EuIRpBecG_h3NZQH_KO9-J8Oz_HjEk",
+  },
 };
 
 export default function RootLayout({
