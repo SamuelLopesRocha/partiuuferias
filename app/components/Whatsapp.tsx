93b894cddebd4fe80d35d4d66cb295fae7817164
Samuel Lopes <samuel.gomes@aluno.faculdadeimpacta.com.br>
2026-05-12 15:08:04 -0300

    Numero alterado

--- app/components/Whatsapp.tsx
+++ app/components/Whatsapp.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function Whatsapp() {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=5511947285115&text=Vim%20do%20site!%20Quero%20fazer%20um%20or%C3%A7amento!"
+      href="https://api.whatsapp.com/send?phone=5511980127335&text=Vim%20do%20site!%20Quero%20uma%20consultoria%20personalizada!"
       target="_blank"
       className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-400 text-white p-4 rounded-full shadow-xl text-2xl z-50"
     >
